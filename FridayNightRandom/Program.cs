aa6887cb2af15a9adaa88263572228edb9ce41aa
Alexandra Efimova <aefimova@tktk.ee>
2021-10-26 23:57:16 +0300

    add new project

---
+++ FridayNightRandom/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FridayNightRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandonMovie();
+        }
+
+        private static int GenerateRandomIndex(string[] someArray)
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, someArray.Length);
+
+            return randomIndex;
+        }
+
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            
+
+            Console.WriteLine($"Computer picked: {foods[GenerateRandomIndex(foods)]}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            
+            Console.WriteLine($"Computer picked: {drinks[GenerateRandomIndex(drinks)]}");
+        }
+        private static void DisplayRandonMovie()
+        {
+            string[] films = { "Venom", "Harry Poter", "1+1", "Tenet" };
+            
+            Console.WriteLine($"Computer picked: {films[GenerateRandomIndex(films)]}");
+        }
+    }
+}
---
+++ FunctionsReturnAndArraysFromFile/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FunctionsReturnAndArraysFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayRandomFood();
+            DisplayRandomDrink();
+            DisplayRandonMovie();
+        }
+
+        private static void DisplayRandomFood()
+        {
+            string[] foods = { "chicken wings", "pizza", "sushi", "popcorn", "salad" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, foods.Length);
+
+            string randomFood = foods[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFood}");
+        }
+
+        private static void DisplayRandomDrink()
+        {
+            string[] drinks = { "apple juice", "coke", "fanta", "sprite" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, drinks.Length);
+
+            string randomDrink = drinks[randomIndex];
+            Console.WriteLine($"Computer picked: {randomDrink}");
+        }
+        private static void DisplayRandonMovie()
+        {
+            string[] films = { "Venom", "Harry Poter", "1+1", "Tenet" };
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, films.Length);
+
+            string randomFilms = films[randomIndex];
+            Console.WriteLine($"Computer picked: {randomFilms}");
+        }
+    }
+}
